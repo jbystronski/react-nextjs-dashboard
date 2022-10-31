a6cf452f02f6f9fa38812749645aac72c263bc75
Jakub Bystroński <jbstr85@gmail.com>
2022-10-31 18:29:05 +0100

    change

--- src/pages/api/db/[action]/[model].js
+++ src/pages/api/db/[action]/[model].js
@@ -8,18 +8,20 @@ export default async (req, res) => {
     const conn =
       Connection.getConnection("default") ||
       (await Connection.create({
-        database: "./../../../lib/db",
+        database: "./lib/db",
         label: "default",
         mode: "no_persist",
       }));
 
     console.log("CONN", conn);
 
+    console.log("DB", path.resolve("./lib/db"));
+
     const q = await Query.create({ connection: conn, url, body });
 
     const data = await q.run();
 
-    console.log("DATA", data);
+    // console.log("DATA", data);
 
     res.status(200).json(data);
   } catch (e) {
