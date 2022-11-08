06db0cb2905605407b418d7c6e522d7f57b275ce
Jakub Bystroński <jbstr85@gmail.com>
2022-11-08 23:40:11 +0100

    changes

--- src/pages/api/fm/map.js
+++ src/pages/api/fm/map.js
@@ -3,7 +3,9 @@ const path = require("path");
 
 export default async (req, res) => {
   try {
-    const result = await map(path.resolve(process.cwd(), req.query.path));
+    const result = await map(
+      path.resolve(process.cwd(), "./" + req.query.path)
+    );
 
     return res.status(200).json(result);
   } catch (error) {
