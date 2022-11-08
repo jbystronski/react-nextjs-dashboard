b382c4ce7a98821399106d0d4f75a4a21aafe9df
Jakub Bystroński <jbstr85@gmail.com>
2022-11-09 00:50:33 +0100

    changes

--- src/lib/utils/node-utils/index.js
+++ src/lib/utils/node-utils/index.js
@@ -114,7 +114,7 @@ exports.map = async (root) => {
   if (!root) throw Error("Missing root argument");
 
   try {
-    const target = path.resolve(root);
+    const target = path.join(process.cwd(), root);
 
     const stat = await fs.stat(target);
 
--- src/pages/api/fm/map.js
+++ src/pages/api/fm/map.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 export default async (req, res) => {
   try {
-    const result = await map(path.join(process.cwd(), req.query.path));
+    const result = await map(req.query.path);
 
     return res.status(200).json(result);
   } catch (error) {
