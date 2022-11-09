04b50e527f6cbbdf1dac2faf7d772ae5a3605348
Jakub Bystroński <jbstr85@gmail.com>
2022-11-09 01:28:10 +0100

    changes

--- src/pages/api/fm/map.js
+++ src/pages/api/fm/map.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 export default async (req, res) => {
   try {
-    console.log("p", path.join(process.cwd(), req.query.path));
-    const result = await map(path.join(process.cwd(), req.query.path));
+    console.log("p", path.resolve(process.cwd(), req.query.path));
+    const result = await map(path.resolve(process.cwd(), req.query.path));
 
     return res.status(200).json(result);
   } catch (error) {
