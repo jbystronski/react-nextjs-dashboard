7c3957eab3288f7ec4e05e46f41fce85c9597515
Jakub Bystroński <jbstr85@gmail.com>
2022-11-09 02:38:54 +0100

    changes

--- src/pages/api/fm/create_folder.js
+++ src/pages/api/fm/create_folder.js
@@ -4,7 +4,8 @@ const path = require("path");
 export default async (req, res) => {
   console.log("browsr only false, creating dir");
   console.log("dir new", req.query.path);
-  console.log("joined", path.resolve(process.cwd(), req.query.path));
+  console.log("resolved", path.resolve(process.cwd(), req.query.path));
+  console.log("joined", path.join(process.cwd(), req.query.path));
   try {
     await create_folder(
       path.join(process.cwd(), req.query.path),
