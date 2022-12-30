5ae9f1d693e066995f4f22ac7f883c142f91e544
Jakub Bystroński <jbstr85@gmail.com>
2022-12-30 01:41:20 +0100

    change

--- src/components/admin/charts/ChartsLayout.js
+++ src/components/admin/charts/ChartsLayout.js
@@ -6,7 +6,7 @@ import OrdersTimeline from "./OrdersTimeline";
 import BestsellersList from "./BestsellersList";
 import UsersList from "./UsersList";
 
-import WeatherWidget from "jb-react-weather-widget";
+import { WeatherWidget } from "jb-react-weather-widget";
 import { useTheme } from "@mui/styles";
 import { useWindowSize } from "core/hooks";
 
@@ -43,8 +43,12 @@ const ChartsLayout = () => {
             }}
           >
             <WeatherWidget
-              apiKey={process.env.weather_app_key}
-              units="metric"
+              defaultLocation="Warsaw"
+              units={{
+                speed: "kmh",
+                temperature: "celsius",
+              }}
+              remember={false}
               theme={{
                 color: {
                   font: {
